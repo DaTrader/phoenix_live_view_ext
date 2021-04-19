732da8969c5fe6060a34133477d8869f8f99cae6
damir <damir@irresident.io>
2021-04-19 18:52:19 +0200

    Initial commit

---
+++ lib/phoenix_live_view_ext.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixLiveViewExt do
+  @moduledoc """
+  Documentation for `PhoenixLiveViewExt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixLiveViewExt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_live_view_ext_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixLiveViewExtTest do
+  use ExUnit.Case
+  doctest PhoenixLiveViewExt
+
+  test "greets the world" do
+    assert PhoenixLiveViewExt.hello() == :world
+  end
+end
